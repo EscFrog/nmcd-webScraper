381b17d5c34ed6d254eb464576282c48903c57f6
Walter Park <neodust223@gmail.com>
2024-02-22 18:13:42 +0900

    Update wanted.py
    쓸데없는 조작을 주석처리하고 곧바로 url에 접근하는 방식으로 변경

--- wanted.py
+++ wanted.py
@@ -11,21 +11,21 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto(root_url)
-time.sleep(2)
+page.goto(f"{root_url}/search?query=flutter&tab=position")
+# time.sleep(2)
 
-page.click("button.Aside_searchButton__Xhqq3")
-# page.locator("button.Aside_searchButton__Xhqq3").click()
-time.sleep(2)
+# page.click("button.Aside_searchButton__Xhqq3")
+# # page.locator("button.Aside_searchButton__Xhqq3").click()
+# time.sleep(2)
 
-page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
-time.sleep(2)
+# page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+# time.sleep(2)
 
-page.keyboard.down("Enter")
-time.sleep(3)
+# page.keyboard.down("Enter")
+# time.sleep(3)
 
-page.click("a#search_tab_position")
-time.sleep(3)
+# page.click("a#search_tab_position")
+# time.sleep(3)
 
 for x in range(5):
   page.keyboard.down("End")
