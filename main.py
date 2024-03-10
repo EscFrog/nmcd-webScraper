d20a30bce91cc469e4ac67cf052a5328bdd178e5
Walter Park <neodust223@gmail.com>
2024-03-10 16:38:10 +0900

    스크랩한 결과를 테이블로 뿌려주는 작업
    그리고 picocss를 적용해서 화면을 예쁘게 꾸밈

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from extractors.wanted import scrape_wanted_jobs
 from save_to_csv import save_to_csv
 
 app = Flask("JobScrapper")
+db = {}
 
 @app.route("/")
 def home():
@@ -11,7 +12,13 @@ def home():
 @app.route("/search")
 def search():
   req_keyword = request.args.get("keyword")
-  wanted_jobs = scrape_wanted_jobs(req_keyword)
+  
+  if req_keyword in db:
+    wanted_jobs = db[req_keyword]
+  else:
+    wanted_jobs = scrape_wanted_jobs(req_keyword)
+    db[req_keyword] = wanted_jobs
+  
   return render_template("search.html", keyword=req_keyword, jobs = wanted_jobs)
 
 app.run("127.0.0.1", debug=True)
